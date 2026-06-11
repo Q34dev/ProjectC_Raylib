5fdd583272c9fcc5fcae8dc45c8d827906d3a969
Q34 <jakub.barcicki@gmail.com>
2026-06-11 16:30:38 +0200

    added placing chips

--- main.c
+++ main.c
@@ -9,9 +9,9 @@
 int windowWidth = 700;
 int windowHeight = 600;
 
-unsigned int cellCountX = 4;
-unsigned int cellCountY = 3;
-float cellSize = 100.f;
+unsigned int cellCountX = 5;
+unsigned int cellCountY = 4;
+float cellSize = 80.f;
 float cellDist = 10.f;
 
 float cellPosX, cellPosY,startCellPosX;
@@ -22,21 +22,30 @@ Vector2 backRectSize, backRectPos;
 
 Vector2** cellPositions;
 float* columnEndPositions;
+int** board;
 
-Texture chipRedTex;
+Texture chipRedTex, chipYellowTex;
+Texture colArrowTex;
+
+int currentPlayerIndex = 1;
 
 static void UpdateDrawFrame(void);
 void CellGridSetup();
-void DrawCell(Vector2 cellPos);
+void DrawCell(Vector2 cellPos, int boardVal);
+int GetFirstEmptyRowIndex(int columnIndex);
+void PlaceChip(int rowIndex, int columnIndex);
 
 int main()
 {
     InitWindow(windowWidth, windowHeight, "Connect4");
 
 	chipRedTex = LoadTexture("resources/connect4_chip_red.png");
+    chipYellowTex = LoadTexture("resources/connect4_chip_yellow.png");
+    colArrowTex = LoadTexture("resources/column_select_arrow.png");
 
     CellGridSetup();
 
+    // set the size and position of the back rectangle
     backRectSize.x = ((cellSize + cellDist) * cellCountX) + 30.f;
     backRectSize.y = ((cellSize + cellDist) * cellCountY) + 20.f;
 	backRectPos.x = (windowWidth * 0.5f) - (backRectSize.x * 0.5f);
@@ -65,39 +74,43 @@ static void UpdateDrawFrame(void)
 
     ClearBackground(RAYWHITE);
     
+    // draw the back rectangle
     DrawRectangle(backRectPos.x, backRectPos.y, backRectSize.x, backRectSize.y, BLUE);
 
-    int selectedColIndex = -1;
-    Vector2 posOffset = { 0.f, 0.f };
-    if (IsMouseButtonDown(0))
-    {
-        int mousePosX = GetMouseX();
-        if (mousePosX > (startCellPosX-(cellSize*0.5f)-cellDist))
-        {
-            for (int col = 0; col < cellCountX; col++)
-            {
-                if (mousePosX < columnEndPositions[col])
-                {
-                    posOffset.y = 10.f;
-                    selectedColIndex = col;
+    // check mouse position and input
+    int mousePosX = GetMouseX();
+    if (mousePosX > (startCellPosX-cellHalf-cellDist))
+    { // the mouse is over the board
+        for (int col = 0; col < cellCountX; col++)
+        { // check all columns
+            if (mousePosX < columnEndPositions[col])
+            { // the mouse is over this column
+
+                // check if there is an empty spot in this column
+                int firstEmptyRow = GetFirstEmptyRowIndex(col);
+                if (firstEmptyRow < 0) break;
+
+                // draw an arrow above the column
+                Vector2 colArrowPos = {cellPositions[0][col].x-cellHalf, cellPositions[0][col].y-130.f};
+                DrawTextureEx(colArrowTex, colArrowPos, 0.f, cellTexScale, BLUE);
+
+                if (IsMouseButtonPressed(0))
+                { // if the left mouse button was pressed this frame
                     
-                    break;
+                    PlaceChip(firstEmptyRow, col);
                 }
+
+                break;
             }
         }
     }
 
+    // draw the cells
     for (int row = 0; row < cellCountY; row++)
     {
         for (int col = 0; col < cellCountX; col++)
         {
-            Vector2 cellPos = cellPositions[row][col];
-            if (selectedColIndex >= 0 && col == selectedColIndex)
-            {
-                cellPos.x += posOffset.x;
-                cellPos.y += posOffset.y;
-            }
-            DrawCell(cellPos);
+            DrawCell(cellPositions[row][col], board[row][col]);
         }
     }
 
@@ -118,15 +131,14 @@ void CellGridSetup()
     cellPosY -= cellHalf * (cellCountY-1);
     cellPosY -= cellDist * 0.5f * (cellCountY-1);
 
+    // allocate memory for the cellPositions array
     int len = sizeof(Vector2*) * cellCountY + sizeof(Vector2) * cellCountX * cellCountY;
     cellPositions = (Vector2**)malloc(len);
     Vector2* ptr = (Vector2*)(cellPositions + cellCountY);
-    
     for (int row = 0; row < cellCountY; row++)
-    {
         cellPositions[row] = (ptr + cellCountX * row);
-    }
 
+    // fill out the cellPositions array
 	for (int row = 0; row < cellCountY; row++)
 	{
 		cellPosX = startCellPosX;
@@ -140,15 +152,57 @@ void CellGridSetup()
 		cellPosY += cellSize + cellDist;
 	}
 
+    // allocate memory for the columnEndPositions array
     columnEndPositions = (float*)malloc(sizeof(float) * cellCountX);
+
+    // fill out the columnEndPositions array
 	for (int col = 0; col < cellCountX; col++)
 	{
 		columnEndPositions[col] = cellPositions[0][col].x + cellHalf + cellDist;
 	}
+
+    // allocate memory for the board array
+    len = sizeof(int*) * cellCountY + sizeof(int) * cellCountX * cellCountY;
+    board = (int**)malloc(len);
+    int* bPtr = (int*)(board + cellCountY);
+    for (int row = 0; row < cellCountY; row++)
+    {
+        board[row] = (bPtr + cellCountX * row);
+        for (int col = 0; col < cellCountX; col++)
+            board[row][col] = 0;
+    }
 }
 
-void DrawCell(Vector2 cellPos)
+void DrawCell(Vector2 cellPos, int boardVal)
 {
-	Vector2 texPos = {cellPos.x - cellHalf, cellPos.y - cellHalf};
-	DrawTextureEx(chipRedTex, texPos, 0.f, cellTexScale, WHITE);
+    if (boardVal == 0)
+    { // no chip
+        DrawCircle(cellPos.x, cellPos.y, cellHalf, DARKBLUE);
+    }
+    else
+    { // red chip (1) or yellow chip (2)
+        
+        Texture tex = boardVal == 1 ? chipRedTex : chipYellowTex;
+        Vector2 texPos = {cellPos.x - cellHalf, cellPos.y - cellHalf};
+
+	    DrawTextureEx(tex, texPos, 0.f, cellTexScale, WHITE);
+    }
+}
+
+int GetFirstEmptyRowIndex(int columnIndex)
+{
+    for (int row = cellCountY-1; row >= 0; row--)
+    {
+        if (board[row][columnIndex] == 0)
+            return row;
+    }
+    return -1;
+}
+
+void PlaceChip(int rowIndex, int columnIndex)
+{
+    board[rowIndex][columnIndex] = currentPlayerIndex;
+
+    // switch the player
+    currentPlayerIndex = currentPlayerIndex == 1 ? 2 : 1;
 }
