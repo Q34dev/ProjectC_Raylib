dcf799d58c4b90f7b2697551123f213d763b0a96
WisniewskiOlaf <olwis198@wp.pl>
2026-06-12 21:33:27 +0200

    save and load functions

--- src/file_manager.c
+++ src/file_manager.c
@@ -55,3 +55,57 @@ void FileManager_ReadConfigFile()
     }
     fclose(file);
 }
+
+void SaveGame(int playerTurnIndex, int cellCountX, int cellCountY, int **board)
+{
+    FILE *saveFile = fopen("files/save.txt", "w");
+    if (saveFile == NULL)
+    {
+        printf("save file not found");
+        return;
+    }
+    fprintf(saveFile, "%d\n", playerTurnIndex);
+    for (int row = 0; row < cellCountY; row++)
+    {
+        for (int col = 0; col < cellCountX; col++)
+        {
+            fprintf(saveFile, "%d", board[row][col]);
+        }
+        fprintf(saveFile, "\n");
+    }
+    fclose(saveFile);
+}
+
+void LoadGame(int *playerTurnIndex, int cellCountX, int cellCountY, int **board)
+{
+    FILE *saveFile = fopen("files/save.txt", "r");
+    if (saveFile == NULL)
+    {
+        printf("save file not found");
+        return;
+    }
+    char fileLine[100];
+    int y = -1;
+    int value;
+    while (fgets(fileLine, 100, saveFile))
+    {
+
+        if (y == -1)
+        {
+            sscanf(fileLine, "%d", &value);
+
+            *playerTurnIndex = (value == 1 || value == 2) ? value : 1;
+            y++;
+            continue;
+        }
+        for (int x = 0; x < cellCountX; x++)
+        {
+            value = (int)fileLine[x] - 48;
+            board[y][x] = (value >= 0 && value <= 9) ? value : 0;
+        }
+        printf("\n");
+        y++;
+    }
+
+    fclose(saveFile);
+}
