3a33b11ace32ec177b34252e96f274d30b5d64ac
Q34 <jakub.barcicki@gmail.com>
2026-06-12 18:51:18 +0200

    improved project hierarchy

---
+++ include/gui.h
@@ -0,0 +1,10 @@
+#include "../src/gui.c"
+#include "raylib.h"
+
+void CellGridSetup();
+void DrawCell(Vector2 cellPos, int boardVal);
+int GetFirstEmptyRowIndex(int columnIndex);
+void PlaceChip(int rowIndex, int columnIndex);
+
+void UpdateDrawFrame();
+void GUI_DrawGame();
