2443c0938f07b8654b6b2febd028f93f17a44ab5
WisniewskiOlaf <olwis198@wp.pl>
2026-06-12 19:53:25 +0200

    wczytywanie z pliku

---
+++ include/file_manager.h
@@ -0,0 +1,6 @@
+#include "../src/file_manager.c"
+
+void ReadConfigFile();
+int StartsWith(const char *str, const char *prefix);
+
+void SetSettings(char *lable, char *setting);
---
+++ src/file_manager.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void ReadConfigFile()
+{
+    FILE *file = fopen("../files/config.txt", "r");
+    if (file == NULL)
+    {
+        printf("config file not found");
+        return;
+    }
+    char fileLine[100];
+    char currentLable[100] = "";
+    while (fgets(fileLine, 100, file))
+    {
+        if (currentLable[0] == '\0')
+        {
+            strcpy(currentLable, fileLine);
+            printf("%s", currentLable);
+        }
+        else
+        {
+            SetSettings(currentLable, fileLine);
+            strcpy(currentLable, "");
+        }
+    }
+}
+int StartsWith(const char *str, const char *prefix)
+{
+    while (*prefix && *str == *prefix)
+        ++str, ++prefix;
+    return *prefix == 0;
+}
+
+void SetSettings(char *lable, char *setting)
+{
+    int value;
+
+    if (StartsWith(lable, "width:"))
+    {
+        sscanf(setting, "%d", &value);
+        printf("set width %d\n", value);
+    }
+    if (StartsWith(lable, "height:"))
+    {
+        sscanf(setting, "%d", &value);
+        printf("set height %d\n", value);
+    }
+}
